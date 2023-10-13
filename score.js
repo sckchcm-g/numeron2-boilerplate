631d187b8f2c6b8373135a9c319e9afc32038efc
Saksham Gupta <141105618+sckchcm-g@users.noreply.github.com>
2023-10-13 15:25:44 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,14 @@
 // Iteration 8: Making scoreboard functional
+var urlParams = new URLSearchParams(window.location.search);
+
+var score = urlParams.get("score");
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+
+playAgainButton.onclick = () => {
+    location.href = "./game.html";
+};
